7b11b045dbfe9e3c9907b19e1ce3409e22d2c40e
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-07-17 08:32:02 +0530

    app.py

--- app.py
+++ app.py
@@ -74,23 +74,20 @@ def user_input(user_question, api_key):
 
 def main():
     st.markdown("")
-    #st.markdown("Upload Appeal:")
     pdf_docs = st.file_uploader("Upload appeal document in PDF format and Click on the Submit & Process Button", accept_multiple_files=True, key="pdf_uploader")
     
-    # if st.button("Classify the document", key="classify_button", help="Click to classify the document"):
-    #     user_question = "Categorize the appeal"
-    #     if user_question and api_key:  # Ensure API key and user question are provided
-    #         user_input(user_question, api_key)
-
     with st.spinner("Processing..."):
         if st.button("Submit & Process", key="process_button", help="Click to submit and process"):
             text = "Appeal: "
             
-            for pdf_path in pdf_docs:
-                pdf_document = fitz.open(pdf_path)
+            for pdf_data in pdf_docs:
+                pdf_bytes = pdf_data.read()
+                pdf_document = fitz.open(stream=pdf_bytes, filetype="bytes")
+                
                 for page_num in range(len(pdf_document)):
                     page = pdf_document.load_page(page_num)
                     images = page.get_images(full=True)
+                    
                     for img_index, img in enumerate(images):
                         xref = img[0]
                         base_image = pdf_document.extract_image(xref)
@@ -98,6 +95,7 @@ def main():
                         image_ext = base_image["ext"] 
                         image = Image.open(io.BytesIO(image_bytes))
                         text += pytesseract.image_to_string(image)
+                
                 pdf_document.close()
                 text += "\n\nResponse 2: "
             
