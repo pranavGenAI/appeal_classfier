7eaa9751c9c3904defc14bb78da4323de729d1bf
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-07-17 13:13:22 +0530

    app.py

--- app.py
+++ app.py
@@ -48,8 +48,8 @@ users = {
 }
 
 def login():
-    col1, col2, col3 = st.columns([1, 1, 1])  # Create three columns with equal width
-    with col2:  # Center the input fields in the middle column
+    col1, col2= st.columns([1, 1])  # Create three columns with equal width
+    with col1:  # Center the input fields in the middle column
         st.title("Login")
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
