f43a17663374dd8b7844d0ee8265e683153fcb78
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-07-17 09:44:14 +0530

    app.py

--- app.py
+++ app.py
@@ -1,45 +1,41 @@
-import pandas as pd
-import numpy as np
 import streamlit as st
-import easyocr
-import PIL
-from PIL import Image, ImageDraw
-from matplotlib import pyplot as plt
+import numpy as np
+from paddleocr import PaddleOCR
+import cv2
 
-# main title
-st.title("Get text from image with EasyOCR") 
-# subtitle
-st.markdown("## EasyOCRR with Streamlit")
+# Initialize PaddleOCR
+ocr = PaddleOCR()
 
-# upload image file
-file = st.file_uploader(label = "Upload your image", type=['png', 'jpg', 'jpeg'])
+def ocr_image(image):
+    # Perform OCR on the image
+    result = ocr.ocr(image)
+    extracted_text = ''
+    for line in result:
+        for word_info in line:
+            extracted_text += word_info[1] + ' '
+        extracted_text += '\n'
+    return extracted_text
 
-image = Image.open(file) # read image with PIL library
-st.image(image) #display
+def main():
+    st.title("Image to Text Conversion with PaddleOCR")
+    st.write("Upload an image and get text from it.")
 
-# it will only detect the English and Turkish part of the image as text
-reader = easyocr.Reader(['tr','en'], gpu=False) 
-result = reader.readtext(np.array(image))  # turn image to numpy array
+    # File uploader widget
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
-textdic_easyocr = {} 
-for idx in range(len(result)): 
-  pred_coor = result[idx][0] 
-  pred_text = result[idx][1] 
-  pred_confidence = result[idx][2] 
-  textdic_easyocr[pred_text] = {} 
-  textdic_easyocr[pred_text]['pred_confidence'] = pred_confidence
+    if uploaded_file is not None:
+        # Read the image
+        image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
 
- # create a dataframe which shows the predicted text and prediction confidence
-  df = pd.DataFrame.from_dict(textdic_easyocr).T
-  st.table(df)
+        # Display the image
+        st.image(image, caption='Uploaded Image', use_column_width=True)
 
-  def rectangle(image, result):
-    # https://www.blog.pythonlibrary.org/2021/02/23/drawing-shapes-on-images-with-python-and-pillow/
-    """ draw rectangles on image based on predicted coordinates"""
-    draw = ImageDraw.Draw(image)
-    for res in result:
-        top_left = tuple(res[0][0]) # top left coordinates as tuple
-        bottom_right = tuple(res[0][2]) # bottom right coordinates as tuple
-        draw.rectangle((top_left, bottom_right), outline="blue", width=2)
-    #display image on streamlit
-    st.image(image)
+        # Perform OCR
+        text = ocr_image(image)
+
+        # Display extracted text
+        st.header("Extracted Text:")
+        st.write(text)
+
+if __name__ == '__main__':
+    main()
